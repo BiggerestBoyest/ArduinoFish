f00825b819e8889a019edcaee65c8630e26ad504
Francisco Castro <francisco.castro3030@gmail.com>
2024-03-20 14:37:11 +1100

    fully implemented timer logic

--- FishMain/FishMain.ino
+++ FishMain/FishMain.ino
@@ -17,17 +17,19 @@ Fish fish;
 GameManager manager;
 Sensors sensors;
 
-unsigned char microSecond = 0;
-unsigned char second = 5;
+//4Digit Display Variables
+int microSecond = 100;
+int internalSecond = 5; // unsigned char cant go below zero, bit of a fix to get around it
+unsigned char second = 5; // the number displayed on the 4 digit display
 unsigned char minute = 2;
-int8_t TimeDisplay[] = {0x00, 0x00, 0x00, 0x00};
+int8_t TimeDisplay[] = {0x00, 0x00, 0x00, 0x00}; 
+bool FLAG_MINUTE = false;
+int FLAG_MINUTE_BLINK_AMOUNT = 8;
 
 bool CanUpdateTimer = true;
 
 Player firstPlayer;
 //Player secondPlayer;
-
-
 
 void setup(){
   Serial.begin(9600);
@@ -53,22 +55,46 @@ void loop() {
     while(!manager.GameStarted){} // doesn't start the game until the manager is ready
     
     if(CanUpdateTimer){
+
         UpdateTimer();
         tm1637.display(TimeDisplay);
-    }
-    
-    Serial.print("test");
-}
+    } else if (minute == 0 && second == 0 && microSecond == 0){
+          tm1637.point(POINT_OFF);    
 
+          TimeDisplay[2] = 0x49;   
+          TimeDisplay[3] = 0x6E;
+          TimeDisplay[0] = 0x99;
+          TimeDisplay[1] = 0x46;
+          tm1637.display(TimeDisplay);
+          delay(500);
+          tm1637.clearDisplay();
+           delay(500);
+    }
+}
 
 void UpdateTimer(){
 
     tm1637.point(POINT_ON);    //POINT_ON = 1,POINT_OFF = 0;
+    second = internalSecond < 0 ? 0 : internalSecond;
 
-    TimeDisplay[2] = second / 10 ;
-    TimeDisplay[3] = second % 10;
-    TimeDisplay[0] = minute / 10;
-    TimeDisplay[1] = minute % 10;
+    if (FLAG_MINUTE == true && FLAG_MINUTE_BLINK_AMOUNT > 0){
+        TimeDisplay[2] = 0x00;
+        TimeDisplay[3] = 0x00;
+        TimeDisplay[0] = 0x6;
+        TimeDisplay[1] = 0x00;
+      delay(200);
+      tm1637.clearDisplay();
+      delay(200);
+      CanUpdateTimer = false;
+      FLAG_MINUTE_BLINK_AMOUNT--;
+      return;
+
+    }    
+    //displays the time, if its less than a minute left, switch to show the microseconds
+    TimeDisplay[2] = minute == 0 ? microSecond / 10 : second / 10 ;
+    TimeDisplay[3] = minute == 0 ? microSecond % 10 : second % 10 ;
+    TimeDisplay[0] = minute == 0 ? second / 10 : minute / 10;
+    TimeDisplay[1] = minute == 0 ? second % 10 : minute % 10;
     CanUpdateTimer = false;
 }
 
@@ -81,14 +107,30 @@ void SetupFourDigitDisplay(){
 }
 
 void TimingISR(){
+    if(minute == 0 && second == 0 && microSecond == 0){
+      CanUpdateTimer = false;
+          // TimeDisplay[2] = 0x49;   
+          // TimeDisplay[3] = 0x6E;
+          // TimeDisplay[0] = 0x99;
+          // TimeDisplay[1] = 0x46;
+          // tm1637.display(TimeDisplay);
+          return;
+        }
+
     microSecond--;
+
+    
     CanUpdateTimer = true;
 
-    if (microSecond <= 0){
-      second--;
-        if (second <= 0){
+    if (microSecond < 0){
+      internalSecond--;
+        if (internalSecond == 0 && minute == 1){
+          FLAG_MINUTE = true;
+        }
+        if (internalSecond < 0){
+          internalSecond--;
           minute--;
-          second = 59;
+          internalSecond = 59;
         }
         microSecond = 100;
     }
